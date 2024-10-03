4d152466cbcd3d93e149c47a6873683c47744d9c
Rob <robwood2806@gmail.com>
2024-10-03 10:03:35 +0200

    first

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,100 @@
+using System;
+using ASPBookProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPBookProject.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    // Nous allons creer un dbset pour chaque table de notre base de donnees
+    // Dbset est une classe generique qui represente une table dans la base de donnees
+    // Elle est responsable du mapping objet-relationnel
+    public DbSet<Student> Roster { get; set; }
+    public DbSet<Instructor> Instructors { get; set; }
+
+    public DbSet<Patient> Patients => Set<Patient>();
+    public DbSet<Medecin> Medecins => Set<Medecin>();
+    public DbSet<Allergie> Allergies => Set<Allergie>();
+    public DbSet<Ordonnance> Ordonnances => Set<Ordonnance>();
+    public DbSet<Medicament> Medicaments => Set<Medicament>();
+    public DbSet<Antecedent> Antecedents => Set<Antecedent>();
+
+
+    // Constructeur
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+
+    // Ajout de mock data
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Patient>()
+          .HasMany(p => p.Allergies)
+          .WithMany(a => a.Patients)
+          .UsingEntity(j => j.ToTable("AllergiePatient")); ;
+
+        modelBuilder.Entity<Patient>()
+            .HasMany(p => p.Antecedents)
+            .WithMany(a => a.Patients)
+            .UsingEntity(j => j.ToTable("AntecedentPatient")); ;
+
+        modelBuilder.Entity<Allergie>()
+            .HasMany(a => a.Medicaments)
+            .WithMany(m => m.Allergies);
+
+        modelBuilder.Entity<Antecedent>()
+            .HasMany(a => a.Medicaments)
+            .WithMany(m => m.Antecedents);
+
+        modelBuilder.Entity<Ordonnance>()
+            .HasOne(o => o.Patient)
+            .WithOne(p => p.Ordonnance)
+            .HasForeignKey<Ordonnance>(o => o.PatientId);
+
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Student>().HasData(
+            new Student()
+            {
+                StudentId = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                AdmissionDate = DateTime.Parse("1990-01-01"),
+                GPA = 3.5,
+                Major = Major.IT,
+                IsVeteran = false
+            });
+        modelBuilder.Entity<Instructor>().HasData(
+            new Instructor()
+            {
+                InstructorId = 1,
+                FirstName = "Jane",
+                LastName = "Doe",
+                IsTenured = true,
+                HiringDate = DateTime.Parse("2010-01-01"),
+                Rank = Ranks.AssociateProfessor
+            },
+            new Instructor()
+            {
+                InstructorId = 2,
+                FirstName = "John",
+                LastName = "Smith",
+                IsTenured = false,
+                HiringDate = DateTime.Parse("2015-01-01"),
+                Rank = Ranks.Instructor
+            },
+            new Instructor()
+            {
+                InstructorId = 3,
+                FirstName = "Jane",
+                LastName = "Smith",
+                IsTenured = true,
+                HiringDate = DateTime.Parse("2012-01-01"),
+                Rank = Ranks.FullProfessor
+            }
+
+            );
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+// set up the basic features of the ASP.NET Core platform
+
+using ASPBookProject.Data;
+using ASPBookProject.Services.FakeDataService;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+
+
+// Enregistrement d'un service 
+builder.Services.AddSingleton<IFakeDataService, FakeDataService>();
+// Assurez vous que cette ligne soit placée avant : var app = builder.Build();
+//builder.Services.AddSingleton<IMyFirstService, MyFirstService>();
+// Nous n'avons maintenant plus à nous soucier à creer une instance de MyFristServie
+// au sein de notre application, le service d'injection de dependance s'en
+// chargera pour nous ! 
+
+var serverVersion = new MySqlServerVersion(new Version(11, 0, 2));
+
+// Ajout du dbcontext au service container
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), serverVersion)
+);
+
+// set up middleware components
+var app = builder.Build();
+
+// Verification que la base de donnees est creee
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<ApplicationDbContext>();
+context.Database.EnsureCreated();
+
+// Par défaut les fichiers contenus au sein du dossier wwwroot ne 
+// sont pas accessible coté client
+// Le composant middleware UseStaticFiles le permet
+app.UseStaticFiles(); // give access to files in wwwroot
+
+// Au sein de notre projet UseStaticFiles() sera appelé avant 
+// UseAuthentication() ce qui permettra l'accès aux fichiers 
+// statiques aux utilisateurs non authentifiés, attention 
+// à ne pas stocker des données sensibles dans le dossier wwwroot
+
+// Pour l'accès aux fichiers statiques vous pouvez utiliser un chemin
+// relatif au web root, par exemple l'acces au fichier wwwroot/images/image01.jpg,
+// on utilisera le chemin http://localhost:5245/images/image01.jpg
+
+
+app.UseRouting();
+
+// Default Routing system
+// app.MapDefaultControllerRoute();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Instructor}/{action=index}/{id?}"
+);
+
+app.Run();
